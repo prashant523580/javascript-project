98f997ad8b630633f54d5610b00789eb936f9f8f
prashant523580 <prashant.thapa62758@gmail.com>
2021-03-01 21:39:33 +0545

    toggling height of nav

---
+++ scroll/main.js
@@ -0,0 +1,13 @@
+const footerDate = document.querySelector(".date");
+
+const d = new Date();
+footerDate.innerHTML = d.getFullYear();
+
+const link_container = document.querySelector(".link-container");
+const link_content = document.querySelector(".nav-link");
+const link = document.querySelector(".nav-links");
+const menu_btn = document.querySelector(".toggle-btn");
+
+menu_btn.addEventListener("click", () =>{
+    link_container.classList.toggle("show-height");
+});
