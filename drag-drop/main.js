71cf95a6c4c1ebf05dbdda653bcf4dc7c1af2875
prashant523580 <prashant.thapa62758@gmail.com>
2021-03-18 03:34:29 +0545

    drag drop content error

--- drag-drop/main.js
+++ drag-drop/main.js
@@ -1,83 +1,69 @@
-let drop_container = document.querySelectorAll(".drop-container")
+let  main_container = document.querySelectorAll(".main-container");
+let drag_container = document.querySelectorAll(".drag-container");
 let draggable = document.querySelectorAll(".draggable");
-let dragItem ;
-draggable.forEach((e) => {
-    e.addEventListener("dragstart", function(evt){
-        dragItem = evt.target;
-        evt.dataTransfer.effectAllowed = 'move';
-        console.log(dragItem)
-        console.log("dragstart");
-        evt.dataTransfer.setData("text/html", evt.target.innerHTML);
-        e.classList.add("dragging");
-    } )
-    e.addEventListener("dragenter", () => {
+let dragged ;
+draggable.forEach((elem) => {
+    elem.addEventListener("dragstart", (e) => {
+            console.log("dragstart");
+            dragged = e.target;
+            e.dataTransfer.effectAllowed = "move";
+            e.dataTransfer.setData("text/html", e.target.innerHTML);
+            setTimeout(() => {
+
+                e.target.style.display = "none";
+            }, 50);
+            // e.target.style.opacity = ".4";
+        }, false);
+        elem.addEventListener("dragend", (e) => {
+            console.log("dragend");
+            // e.target.style.opacity = "1";
+           
+
+        }, false);
+        // elem.addEventListener("drop", (e)  => {
+        //     e.preventDefault()
+        //     dragged = e.target.innerHTML;
+        //    e.target.innerHTML = e.dataTransfer.getData("text/html");
+        // })
+});
+main_container.forEach((elem) => {
+    elem.addEventListener("dragover", (e) => {
+        e.preventDefault();
+        e.dataTransfer.dropEffect = "move";
+        e.target.style.border = "2px solid green";
+    },false);
+    elem.addEventListener("dragenter", (e) => {
+        e.target.style.border = "2px solid green";
         console.log("drag enter");
-    })
-    e.addEventListener("dragover", (evt) => {
-        console.log("over")
-        evt.preventDefault();
-        evt.dataTransfer.dropEffect = "move";
-    })
-    e.addEventListener("dragleave", (evt) => {
-        console.log("drag leave")
-        evt.stopPropagation();
-    })
-    e.addEventListener("drop", (evt) => {
-        dragItem.innerHTML = evt.target.innerHTML;
-        evt.preventDefault();
-        evt.target.innerHTML =  evt.dataTransfer.getData("text/html");
-    })
-    e.addEventListener("dragend", function () {
-        e.classList.remove("dragging");
-        console.log("drag end")
-    })
+    }, false)
+    elem.addEventListener("dragleave" , (e) => {
+        e.target.style.border = "none";
+        e.stopPropagation();
+        e.dataTransfer.clearData("text/html");
+        // e.dataTransfer.dropEffect = "move";
+    }, false);
+    elem.addEventListener("drop", (e) => {
+        e.preventDefault();
+        let  data = e.dataTransfer.getData("text/html");
+        let element = document.createElement("div");
+        dragged.innerHTML = e.target.innerHTML;
+        element.className = "draggable";
+        element.setAttribute("draggable", "true");
+        console.log(data)
+        element.innerHTML = data;
+        console.log(element);
+        // console.log(element);
+        if(data === ""){
+            console.log(" none");
+            e.dataTransfer.effectAllowed = "move";
+            dragged.innerHTML = e.target;
+            data = e.dataTransfer.setData("text/html", e.target.innerHTML);
+            console.log("data : " ,data);
+            
+        }else{
+            
+            e.target.append(element);
+        }
+
+    }, false);
 })
-
-// function isListItems(evt){
-//     return evt.target && evt.target.classList.contains("draggable");
-
-// }
-// function onDragstart(evt){
-//     if(isListItems(evt)){
-//         dragItem = evt.target;
-//         console.log(dragItem);
-//         evt.dataTransfer.effectAllowed = "move";
-//         evt.dataTransfer.setData('text/html',
-//          evt.target.innerHTML);
-//     }
-// }
-// function onDragenter(evt){
-//     if(isListItems(evt)){
-//         console.log("drag enter", evt)
-//     }
-// }
-// function onDragleave(evt){
-//     if(isListItems(evt)){
-//         evt.stopPropagation();
-//         console.log("drag leave")
-//     }
-// }
-// function onDragover(evt){
-//     if(isListItems(evt)){
-//         evt.preventDefault();
-//         evt.dataTransfer.dropEffect = "move";
-//     }
-// }
-// function onDrop(evt){
-//     if(isListItems(evt)){
-//         dragItem.innerHTML = evt.target.innerHTML;
-//         evt.preventDefault();
-//         evt.target.innerHTML = evt.dataTransfer.getData("text/html");
-//     }
-// }
-// function onDragend(){
-
-// }
-// drop_container.forEach( (e) => {
-//     e.addEventListener("dragstart",onDragstart );
-//     e.addEventListener("dragenter", onDragenter);
-//     e.addEventListener("dragover", onDragover);
-//     e.addEventListener("drop", onDrop);
-//     e.addEventListener("dragleave", onDragleave);
-//     e.addEventListener("dragend", onDragend);
-// })
