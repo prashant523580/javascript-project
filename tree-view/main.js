a6143dc8dd99ceeb2037c1683ca4f73242337d57
prashant523580 <prashant.thapa62758@gmail.com>
2021-03-20 19:08:00 +0545

    fixed drag and drop

--- tree-view/main.js
+++ tree-view/main.js
@@ -1,11 +1,13 @@
 let dropable = document.querySelectorAll(".dropable");
 let draggable = document.querySelectorAll("ul li");
-console.log(draggable)
-
+let dragged;
 draggable.forEach((elem) => {
     elem.addEventListener("dragstart", (e) => {
         console.log("drag-start");
+        dragged = e.target;
+        e.dataTransfer.effectAllowed = "move";
         e.dataTransfer.setData("text", e.target.id);
+        e.dataTransfer.setData("text/html", e.target.innerHTML);
     })
     elem.addEventListener("dragend", (e) => {
         console.log("drag-end")
@@ -17,12 +19,81 @@ dropable.forEach((elem) => {
         e.preventDefault();
     })
     elem.addEventListener("dragleave", (e) => {
-        console.log("dragleave", e.target);
+        // console.log("dragleave", e.target);
+        e.dataTransfer.dropEffect = "move";
     })
     elem.addEventListener("drop", (e)=> {
         e.preventDefault();
         let data = e.dataTransfer.getData("text");
-        e.target.appendChild(document.getElementById(data));
+        if(e.target.childNodes.length > 1){
+            e.target.parentNode.childNodes[5].appendChild(document.getElementById(data));
+        }else{
+            e.dataTransfer.dropEffect ="move";
+            // e.target.innerHTML = e.target.childNodes[0];
+            // console.log(e.target.childNodes[0].nodeValue)
+            if(dragged){
+                console.log(e.target.innerHTML)
+                console.log(dragged)
+
+                dragged.innerHTML = e.target.innerHTML;
+                e.target.innerHTML = e.dataTransfer.getData("text/html");
+                
+            }
+        }
+        
     } )
 })
+sortFun = (ul) => {
+    console.log(ul)
+        var elements = Array.from(ul.children);
+        console.log(elements);
+        elements.sort((a,b) =>{
+            var  list1 = a.getAttribute('data-char').charCodeAt(0);
+            var list2 = b.getAttribute('data-char').charCodeAt(0);
+            let charCA = 'a'.charCodeAt(0);
+            //add weight if its a number
+                if(list1 < charCA) list1 += 100;
+                if(list2 < charCA) list2 += 100;
 
+                return list1 -list2;
+        } )
+
+        //append back to update the order
+        elements.forEach((element) =>  {
+            ul.appendChild(element)
+        })
+      
+
+}
+
+// function sortFun(){
+//     var sortFlag, sorted,i,data_list;
+//     let main_container = document.querySelector("#main-ul");
+//     sortFlag = true;
+//     while(sortFlag){
+//         sortFlag = false;
+//         data_list = main_container.getElementsByTagName("LI");
+
+//         for(i=0; i < data_list.length - 1; i++){
+//             sorted = false;
+//             let data1 = data_list[i].innerHTML.toLowerCase();
+//             let data2 = data_list[i + 1].innerHTML.toLowerCase();
+//             if(data1 > data2){
+//                 sorted = true;
+//                 break;
+//             }
+//             if(sorted){
+//                 data_list[i].parentNode.insertBefore(data_list[i + 1], data_list[i]);
+//                 sortFlag = true;
+//             }
+//             Array.from(data_list).forEach((elem) => {
+//                 let list_num = elem.childNodes[0];
+//                 let list_array = [list_num];
+//                 list_array.sort((a,b) => {
+//                     return a - b;
+//                 })
+//                 console.log(list_array[0].nodeValue)
+//             })
+//         }
+//     }
+// }
