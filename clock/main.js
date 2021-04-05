38ad664763acc6e9ab4afdd9df9f55c64910a2ac
prashant523580 <prashant.thapa62758@gmail.com>
2021-04-06 04:50:30 +0545

    digital clock

---
+++ clock/main.js
@@ -0,0 +1,31 @@
+
+function updateClock(){
+    
+    // get current time
+    let dateTime = new Date();
+
+    //extract  hour, minute , and second from date
+    let currentHour = dateTime.getHours();
+    // console.log(currentHour);
+    let currentMinute = dateTime.getMinutes();
+    let currentSecond = dateTime.getSeconds();
+    //pad 0 if hour minute second is less then 10
+     currentHour = (currentHour > 12) ? currentHour - 12 :"0"+ currentHour;
+     currentHour = (currentHour == 0 ) ? 12 : currentHour;
+     currentMinute = (currentMinute < 10 ? "0" : '') + currentMinute;
+     currentSecond = (currentSecond < 10 ? "0" : '') + currentSecond;
+    //  converted railway clock to am/pm clock
+    let currentMeridiem = (currentHour < 12) ? "A.M" : "P:M";
+    // let currentTimeStr = currentHour + " : "+ currentMinute + " : " + currentSecond + currentMeridiem;
+        document.getElementById("hours").innerHTML =  "" + currentHour;
+        document.getElementById("minutes").innerHTML = ""+currentMinute;
+        document.getElementById("seconds").innerHTML = ""+currentSecond;
+    document.getElementById("meridiem").innerHTML = currentMeridiem;
+    // document.getElementById("clock").innerHTML = currentTimeStr;
+}
+window.addEventListener("load", () => {
+
+    setInterval(()=>{
+        updateClock();
+    },1000);
+})
