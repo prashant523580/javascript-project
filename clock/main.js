a9f70dc6f413f62b6173e8313617db941caf5eb7
prashant523580 <prashant.thapa62758@gmail.com>
2021-04-06 13:18:17 +0545

    fixed error

--- clock/main.js
+++ clock/main.js
@@ -6,16 +6,16 @@ function updateClock(){
 
     //extract  hour, minute , and second from date
     let currentHour = dateTime.getHours();
-    // console.log(currentHour);
+    console.log(currentHour);
+    //  converted railway clock to am/pm clock
+    let currentMeridiem = (currentHour < 12) ? "A.M" : "P:M";
     let currentMinute = dateTime.getMinutes();
     let currentSecond = dateTime.getSeconds();
     //pad 0 if hour minute second is less then 10
-     currentHour = (currentHour > 12) ? currentHour - 12 :"0"+ currentHour;
-     currentHour = (currentHour == 0 ) ? 12 : currentHour;
+     currentHour = (currentHour > 12) ?  currentHour - 12 :"0"+ currentHour;
+     currentHour = (currentHour == 0 ) ? 12 : "0" +  currentHour;
      currentMinute = (currentMinute < 10 ? "0" : '') + currentMinute;
      currentSecond = (currentSecond < 10 ? "0" : '') + currentSecond;
-    //  converted railway clock to am/pm clock
-    let currentMeridiem = (currentHour < 12) ? "A.M" : "P:M";
     // let currentTimeStr = currentHour + " : "+ currentMinute + " : " + currentSecond + currentMeridiem;
         document.getElementById("hours").innerHTML =  "" + currentHour;
         document.getElementById("minutes").innerHTML = ""+currentMinute;
