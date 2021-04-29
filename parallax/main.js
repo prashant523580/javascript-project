df5de027c8e1df39868b71036dc5f53768b4fde8
prashant523580 <prashant.thapa62758@gmail.com>
2021-04-29 22:04:16 +0545

    parallax content

--- parallax/main.js
+++ parallax/main.js
@@ -13,16 +13,18 @@ let single_bird_3 = document.getElementById("single-bird-3");
 let single_bird_4 = document.getElementById("single-bird-4");
 let single_bird_5 = document.getElementById("single-bird-5");
 let single_bird_6 = document.getElementById("single-bird-6");
+let single_bird_8 = document.getElementById("single-bird-8");
 let ballon = document.getElementById("ballon");
 let small_ballon = document.getElementById("small-ballon");
-
+let forest_road = document.getElementById("forest-road");
+let section2_parallax = document.getElementById("forest-road2");
 
 window.addEventListener("scroll" , () => {
     let y = window.scrollY;
     // console.log(50 + y * 0.05)
     sky.style.top =   y * 0.08 + "%";
     text.style.top =  30 + y * -0.015 +"%";
-    center_mountain.style.top =  -25 + y * 0.12 + "%";
+    center_mountain.style.top =  -15 + y * 0.32 + "%";
     left_mountain.style.left = 10+ y * -0.05 + "%";
     left_mountain.style.top = -3 + y * 0.075 + "%";
     right_mountain.style.top =  y * 0.075 + "%";
@@ -31,14 +33,24 @@ window.addEventListener("scroll" , () => {
     // bottom_sky.style.top = 1 + y * -0.1 + "%";
     single_bird_1.style.left =  -20+ y * 0.03 + "%";
     single_bird_2.style.left = 20 + y * -0.05 + "%";
-    single_bird_3.style.left = y* 0.04 + "%";
-    single_bird_3.style.top = y* 0.04 + "%";
+    single_bird_3.style.left = y * 0.04 + "%";
+    single_bird_3.style.top = y * 0.08 + "%";
     // birds.style.rotate= y * 0.05 + "deg";
-    single_bird_4.style.left =  50+ y * 0.03 + "%";
+    single_bird_4.style.left =  5+ y * 0.03 + "%";
     single_bird_5.style.left = 20 + y * -0.05 + "%";
-    single_bird_6.style.left =  -30 + y* 0.04 + "%";
-    ballon.style.top = 10 + y * 0.04 + "%";
-    ballon.style.left = -20 + y * 0.05 + "%";
-    small_ballon.style.top = 20 + y * -0.06 + "%";
-    small_ballon.style.left = 30 + y * -0.06 + "%";
+    // single_bird_6.style.left =  -30 + y* 0.04 + "%";
+    ballon.style.top = 30 + y * -0.004 + "%";
+    ballon.style.left = 10+ y * 0.05 + "%";
+    small_ballon.style.transform = `scale(${0.01 +y * 0.0054 })`;
+    small_ballon.style.top = 25 + y * -0.04 + "%";
+    small_ballon.style.left = 30 + y * -0.02 + "%";
+    single_bird_8.style.left = 50+  y * -0.05 + "%";
+    if(y > 200){
+        forest_road.style.transform = `scale(${y * 0.0054})`;
+        forest_road.style.top = y * -0.05 + "%";
+    }
+    if(y > 600){
+        section2_parallax.style.transform = `scale(${y * 0.0054})`;
+    }
 })
+
